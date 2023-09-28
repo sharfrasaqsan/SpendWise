7154a7f3477e9724fcf495b7574cdb33a9d19ad4
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2023-09-29 00:52:06 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const expenseForm = document.getElementById("expense-form");
   const expenseList = document.getElementById("expenseList");
-  const expenseCategorySelect = document.getElementById("expenseCategory");
   const totalAmountSpan = document.getElementById("totalAmount");
   const categoryTotalsDiv = document.getElementById("categoryTotals");
   const newCategoryInput = document.getElementById("newCategory");
-
   const expenses = [];
+  let categories = {};
 
   expenseForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -15,19 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const expenseAmount = parseFloat(
       document.getElementById("expenseAmount").value
     );
-    const selectedCategory = expenseCategorySelect.value;
-    const newCategory = newCategoryInput.value;
+    const expenseCategory = getSelectedCategory();
 
-    if (
-      expenseName === "" ||
-      isNaN(expenseAmount) ||
-      (selectedCategory === "" && newCategory === "")
-    ) {
+    if (expenseName === "" || isNaN(expenseAmount) || !expenseCategory) {
       alert("Please fill in all fields and select a category.");
       return;
     }
-
-    const expenseCategory = newCategory || selectedCategory;
 
     const expense = {
       name: expenseName,
@@ -37,101 +29,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
     expenses.push(expense);
 
-    const totalAmount = expenses.reduce(
-      (sum, expense) => sum + expense.amount,
-      0
-    );
-    totalAmountSpan.textContent = `$${totalAmount.toFixed(2)}`;
-
-    updateCategoryTotals(expenses);
-
+    updateCategoryTotals();
+    renderExpenseList();
     expenseForm.reset();
-
-    const newRow = document.createElement("tr");
-    newRow.innerHTML = `
-          <td>${expense.name}</td>
-          <td>$${expense.amount.toFixed(2)}</td>
-          <td>${expense.category}</td>
-          <td>
-              <i class="fas fa-edit edit-icon"></i>
-              <i class="fas fa-trash-alt delete-icon"></i>
-          </td>
-      `;
-    expenseList.appendChild(newRow);
   });
 
-  expenseList.addEventListener("click", function (e) {
-    if (e.target.classList.contains("edit-icon")) {
-      editExpense(e.target);
-    } else if (e.target.classList.contains("delete-icon")) {
-      deleteExpense(e.target);
+  function getSelectedCategory() {
+    const selectedCategory = document.getElementById("expenseCategory").value;
+    const newCategory = newCategoryInput.value.trim();
+
+    if (newCategory !== "") {
+      return newCategory;
     }
-  });
 
-  function editExpense(icon) {
-    const row = icon.closest("tr");
-    const cells = row.getElementsByTagName("td");
-    const expenseName = cells[0].textContent;
-
-    // Find the index of the original expense in the expenses array
-    const expenseIndex = expenses.findIndex(
-      (expense) => expense.name === expenseName
-    );
-
-    if (expenseIndex !== -1) {
-      // Get the original expense
-      const originalExpense = expenses[expenseIndex];
-
-      // Update the form fields with the original expense details
-      document.getElementById("expenseName").value = originalExpense.name;
-      document.getElementById("expenseAmount").value = originalExpense.amount;
-      document.getElementById("expenseCategory").value =
-        originalExpense.category;
-
-      // Remove the row from the table
-      row.remove();
+    if (selectedCategory !== "") {
+      return selectedCategory;
     }
+
+    return null;
   }
 
-  function deleteExpense(icon) {
-    const confirmation = confirm(
-      "Are you sure you want to delete this expense?"
-    );
-
-    if (confirmation) {
-      const row = icon.closest("tr");
-      const cells = row.getElementsByTagName("td");
-      const expenseName = cells[0].textContent;
-      const expenseAmount = parseFloat(cells[1].textContent.replace("$", ""));
-      const expenseCategory = cells[2].textContent;
-
-      const expenseIndex = expenses.findIndex(
-        (expense) => expense.name === expenseName
-      );
-      if (expenseIndex !== -1) {
-        expenses.splice(expenseIndex, 1);
-      }
-
-      const totalAmount = expenses.reduce(
-        (sum, expense) => sum + expense.amount,
-        0
-      );
-      totalAmountSpan.textContent = `$${totalAmount.toFixed(2)}`;
-
-      updateCategoryTotals(expenses);
-
-      row.remove();
-    }
-  }
-
-  function updateCategoryTotals(expenses) {
-    const categories = {};
+  function updateCategoryTotals() {
+    categories = {};
 
     for (const expense of expenses) {
       if (categories[expense.category]) {
         categories[expense.category] += expense.amount;
       } else {
         categories[expense.category] = expense.amount;
+      }
+    }
+
+    // Remove categories with a total of 0
+    for (const category in categories) {
+      if (categories[category] === 0) {
+        delete categories[category];
       }
     }
 
@@ -142,7 +74,117 @@ document.addEventListener("DOMContentLoaded", function () {
       categoryTotalDiv.innerHTML = `<p>${category}: $${categoryTotal.toFixed(
         2
       )}</p>`;
+      categoryTotalDiv.classList.add("category-total");
       categoryTotalsDiv.appendChild(categoryTotalDiv);
     }
   }
+
+  function renderExpenseList() {
+    expenseList.innerHTML = "";
+
+    for (let i = 0; i < expenses.length; i++) {
+      const expense = expenses[i];
+
+      const newRow = document.createElement("tr");
+      newRow.innerHTML = `
+          <td>${expense.name}</td>
+          <td>$${expense.amount.toFixed(2)}</td>
+          <td>${expense.category}</td>
+          <td>
+              <i class="fas fa-edit edit-icon"></i>
+              <i class="fas fa-trash-alt delete-icon"></i>
+          </td>
+      `;
+
+      expenseList.appendChild(newRow);
+    }
+
+    updateTotalAmount();
+  }
+
+  function updateTotalAmount() {
+    const totalAmount = expenses.reduce(
+      (sum, expense) => sum + expense.amount,
+      0
+    );
+    totalAmountSpan.textContent = `$${totalAmount.toFixed(2)}`;
+  }
+
+  expenseList.addEventListener("click", function (e) {
+    if (e.target.classList.contains("edit-icon")) {
+      const editIcon = e.target;
+      const rowIndex = getIndex(editIcon);
+
+      if (rowIndex !== -1) {
+        editExpense(rowIndex);
+      }
+    } else if (e.target.classList.contains("delete-icon")) {
+      const deleteIcon = e.target;
+      const rowIndex = getIndex(deleteIcon);
+
+      if (rowIndex !== -1) {
+        deleteExpense(rowIndex);
+      }
+    }
+  });
+
+  function getIndex(element) {
+    let index = -1;
+    let currentElement = element.parentElement;
+
+    while (currentElement !== null) {
+      if (currentElement.tagName === "TR") {
+        index = Array.from(currentElement.parentElement.children).indexOf(
+          currentElement
+        );
+        break;
+      }
+      currentElement = currentElement.parentElement;
+    }
+
+    return index;
+  }
+
+  function editExpense(index) {
+    const editedExpense = expenses[index];
+
+    document.getElementById("expenseName").value = editedExpense.name;
+    document.getElementById("expenseAmount").value = editedExpense.amount;
+
+    const categorySelect = document.getElementById("expenseCategory");
+    categorySelect.value = editedExpense.category;
+
+    // Remove the edited expense from the expenses array
+    expenses.splice(index, 1);
+
+    // Re-render the expense list
+    renderExpenseList();
+  }
+
+  function deleteExpense(index) {
+    const confirmation = confirm(
+      "Are you sure you want to delete this expense?"
+    );
+
+    if (confirmation) {
+      const deletedExpense = expenses[index];
+
+      // Update the sub-total amount for the deleted category
+      categories[deletedExpense.category] -= deletedExpense.amount;
+
+      // Remove the deleted expense from the expenses array
+      expenses.splice(index, 1);
+
+      // Re-render the expense list
+      renderExpenseList();
+
+      // Update the subtotals display
+      updateCategoryTotals();
+
+      // Update the total amount display
+      updateTotalAmount();
+    }
+  }
+
+  renderExpenseList();
 });
